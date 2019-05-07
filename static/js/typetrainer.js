45c4e8c1b23ae71cd548cf7b990f82e3d662a6d6
BrRoman <editeur@traditions-monastiques.com>
2019-05-07 20:56:47 +0200

    Random sentences.

--- static/js/typetrainer.js
+++ static/js/typetrainer.js
@@ -1,3 +1,21 @@
 $("document").ready(function(){
-    //
+    write_line($("#select_lesson option:selected").text());
+
+    $("#select_lesson").change(function(){
+        write_line($("#select_lesson option:selected").text());
+    });
+
+    $("#change_sentence").click(function(){
+        write_line($("#select_lesson option:selected").text());
+    });
 });
+
+function write_line(letters){
+    letters = letters + " ";
+    var letters_split = letters.split("");
+    var text = "";
+    for(var i = 0; i < 100; i++){
+        text += letters_split[Math.floor(Math.random() * letters_split.length)];
+    }
+    $("#sentence").text(text);
+}
