f6769cfce4c50cbe581b0e8ea3f1f622b2a42774
BrRoman <editeur@traditions-monastiques.com>
2019-05-08 01:59:46 +0200

    First character highlighted.

--- static/js/typetrainer.js
+++ static/js/typetrainer.js
@@ -5,7 +5,7 @@ $("document").ready(function(){
         write_line($("#select_lesson option:selected").text());
     });
 
-    $("#change_sentence").click(function(){
+    $("#reset_sentence").click(function(){
         write_line($("#select_lesson option:selected").text());
     });
 });
@@ -13,9 +13,10 @@ $("document").ready(function(){
 function write_line(letters){
     letters = letters + " ";
     var letters_split = letters.split("");
-    var text = "";
-    for(var i = 0; i < 100; i++){
-        text += letters_split[Math.floor(Math.random() * letters_split.length)];
+    var text = "<span id='char_0' class='char_current'>" + letters_split[Math.floor(Math.random() * (letters_split.length - 1))] + "</span>";
+    for(var i = 1; i <= 100; i++){
+        character = letters_split[Math.floor(Math.random() * letters_split.length)];
+        text += ("<span id='char_" + i + "' class='char_normal'>" + character + "</span>");
     }
-    $("#sentence").text(text);
+    $("#sentence").html(text).off("keypress");
 }
